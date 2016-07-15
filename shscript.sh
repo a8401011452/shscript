40e6b184df4cb495caacb18314dfcfd495d9d548
a8401011452 <weaver122@gmail.com>
2016-07-15 09:06:16 +0800

    first commit

--- shscript.sh
+++ shscript.sh
@@ -8,7 +8,8 @@ apt-key add fox-crypto-gpg.asc
 apt-get update
 cd /root
 #installing normal openvpn, easy rsa & openvpn-nl
-apt-get install openvpn easy-rsa -y
+apt-get install easy-rsa -y
+apt-get install openvpn -y
 apt-get install openvpn-nl -y
 #ipforward
 sysctl -w net.ipv4.ip_forward=1
